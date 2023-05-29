42ba83e6fedf026649181bf68c8bfe2402f83ec0
mustapha amin <mustaphaamin2003@gmail.com>
2023-05-29 16:03:16 -0700

    created the shimmer

---
+++ lib/widgets/shimmer_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ShimmerWidget extends StatefulWidget {
+  double? width, height;
+  BoxShape? shapeBorder;
+  ShimmerWidget({this.height, this.width, this.shapeBorder});
+
+  @override
+  State<ShimmerWidget> createState() => _ShimmerWidgetState();
+}
+
+class _ShimmerWidgetState extends State<ShimmerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      period: const Duration(milliseconds: 300),
+      baseColor: Colors.grey[600]!,
+      highlightColor: Colors.grey[500]!,
+      child: Container(
+        width: widget.width,
+        height: widget.height,
+        decoration: BoxDecoration(
+          shape: widget.shapeBorder!,
+          color: Colors.grey,
+          borderRadius: widget.shapeBorder == BoxShape.circle
+              ? null
+              : BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+}
