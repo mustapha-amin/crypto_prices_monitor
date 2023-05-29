84aa8c78c8099e6bd63a5f466c4566c55f162c98
mustapha amin <mustaphaamin2003@gmail.com>
2023-05-29 16:03:32 -0700

    created the loading shimmer

---
+++ lib/widgets/loading_shimmer.dart
@@ -0,0 +1,60 @@
+import 'package:countries_info/widgets/shimmer_widget.dart';
+import 'package:flutter/material.dart';
+
+class LoadingShimmer extends StatefulWidget {
+  const LoadingShimmer({super.key});
+
+  @override
+  State<LoadingShimmer> createState() => _LoadingShimmerState();
+}
+
+class _LoadingShimmerState extends State<LoadingShimmer> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Card(
+          color: Colors.grey[900],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ShimmerWidget(
+                  height: size.height / 8,
+                  width: size.width / 10,
+                  shapeBorder: BoxShape.circle,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ShimmerWidget(
+                      width: size.width / 3,
+                      height: 6,
+                      shapeBorder: BoxShape.rectangle,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ShimmerWidget(
+                      width: size.width / 5,
+                      height: 4,
+                      shapeBorder: BoxShape.rectangle,
+                    ),
+                  ],
+                ),
+                ShimmerWidget(
+                  width: size.width / 3,
+                  height: 5,
+                  shapeBorder: BoxShape.rectangle,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
