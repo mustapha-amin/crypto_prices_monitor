7b4d055361c886abeb9f85a43fa463b543647fa9
mustapha amin <mustaphaamin2003@gmail.com>
2023-05-29 16:05:25 -0700

    updated some variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
-import 'package:flutter/material.dart';
 import 'package:countries_info/screens/home.dart';
+import 'package:countries_info/widgets/loading_shimmer.dart';
+import 'package:flutter/material.dart';
+import 'package:countries_info/screens/trending.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: HomePage(),
-      debugShowCheckedModeBanner: false,
-    )
-  );
+  runApp(MaterialApp(
+    theme: ThemeData(
+      useMaterial3: true,
+      scaffoldBackgroundColor: Colors.black,
+      tabBarTheme: TabBarTheme(
+        overlayColor: MaterialStateColor.resolveWith((states) => Colors.black),
+      ),
+    ),
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
-
